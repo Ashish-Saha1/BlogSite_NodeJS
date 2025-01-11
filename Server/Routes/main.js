2eb8fb73de7e0adcd4aee2ba0babc0fda5aa08e8
ashish-saha <rubelsaha1@gmail.com>
2025-01-11 22:51:46 +0600

    Titorial -5 onging from Home

--- Server/Routes/main.js
+++ Server/Routes/main.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../../Server/Models/Post');
 
-router.get("/", (req,res)=>{
-    const locals = {
-        title : "Index Page",
-        description : "NodeJS blog site with mongoDb & Express"
+
+router.get("/", async (req,res)=>{
+    try {
+        const locals = {
+            title : "Index Page",
+            description : "NodeJS blog site with mongoDb & Express"
+        }
+
+        const data = await Post.find()
+
+        res.render('index', { locals, data })
+    } catch (error) {
+        console.log(error);
+        
     }
-    res.render('index', { locals })
 })
+
+
 
 router.get("/about", (req,res)=>{
     const locals = {
@@ -16,6 +28,31 @@ router.get("/about", (req,res)=>{
     }
     res.render('about', { locals })
 })
+
+
+
+// function postData(){
+//     Post.insertMany([
+//         {
+//             title : 'Building API with Node JS',
+//             body : 'Learn how to learn Node Js'
+//         },
+//         {
+//             title : 'Deployment Node JS',
+//             body : 'Understand the different way of Node js'
+//         },
+//         {
+//             title : 'Authentication & Authoraization in Node JS',
+//             body : 'How to work Node JS & Mongo DB'
+//         },
+//         {
+//             title : 'Express JS',
+//             body : 'Learn Express JS from LWS'
+//         }
+//     ])
+// }
+
+// postData()
 
 
 module.exports = router;
